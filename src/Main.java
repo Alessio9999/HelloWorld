5adafb77f0a4355cb6e9bf8535ffab7a90c4a77f
LeXX <lexx8665@yandex.ru>
2023-10-06 06:01:38 +0500

    Поменяли вывод

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
-        System.out.println("It is my first application on JAVA!");
+        System.out.println("Привет мир!;-)");
+        System.out.println("Это мое первое приложение на JAVA!:-)");
     }
 }
